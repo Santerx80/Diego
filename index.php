afea80538945854efa18e8dbfe512037206f64b5
Manuel Santiago <santibece232@gmail.com>
2024-12-11 17:34:44 -0500

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo '<h1>Hello World</h1>';
+?>
+
+<style>
+    body {
+        background-color: rgb(0 0 0 / .7);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
